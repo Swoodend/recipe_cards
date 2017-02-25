2635f1ec69b9be10d218f522749f73c0f4d2a3e2
Scott Woodend <woodendscott@gmail.com>
2017-02-25 17:42:02 -0500

    recipe cards render a CardBody component

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import RecipeHeader from './RecipeHeader';
+import CardBody from './CardBody';
+
+//this.props.num is the card number
 
 class RecipeCard extends Component {
   render(){
+    console.log('this.props.num', this.props.num);
     return (
       <div className="max-width">
         <div className="recipe-card">
           {this.props.children}
           <RecipeHeader title={this.props.title}/>
+          <CardBody />
         </div>
       </div>
     );
