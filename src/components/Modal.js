26edf4c988e29dc27d6a33e2ff248f71085b0f45
Scott Woodend <woodendscott@gmail.com>
2017-02-25 13:14:15 -0500

    Modal should be its own component

---
+++ src/components/Modal.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    let recipeTitle = this.refs.newRecipe.value;
+    e.preventDefault();
+    console.log('you submitted the form');
+    this.props.exitModalMode();
+    this.props.addNewRecipe(recipeTitle);
+  }
+  render(){
+    return (
+      <div className="modal">
+        <h3>Create a new recipe</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input ref="newRecipe" className="new-recipe-input" type="text"/>
+          <button className="save" type="submit">Save</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Modal;
