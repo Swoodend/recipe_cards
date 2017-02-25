9e53eb3b36e9293470e05f99b780e9f99dcf433d
Scott Woodend <woodendscott@gmail.com>
2017-02-25 15:55:30 -0500

    create DeleteButton component

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DeleteButton extends Component {
+  render(){
+    return (
+      <div className="delete-button">
+        x
+      </div>
+    );
+  }
+}
+
+export default DeleteButton;
