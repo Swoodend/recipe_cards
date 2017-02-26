aa1477afacd29a9043cc4da2d430ecf69eaeb215
Scott Woodend <woodendscott@gmail.com>
2017-02-25 20:32:44 -0500

    clean up AddIngredientsInput form

--- src/components/AddIngredientInput.js
+++ src/components/AddIngredientInput.js
@@ -16,15 +16,13 @@ class AddIngredientInput extends Component {
 
   render() {
     return (
-      <div style={{position: 'relative', height: '75px'}}>
-        <form className="add-ingredient-form" onSubmit={this.handleSubmit.bind(this)}>
-          <input
-            className="add-ingredient-input"
-            placeholder="click to add ingredient"
-            ref="newIngredient"
-          />
-        </form>
-      </div>
+      <form className="add-ingredient-form" onSubmit={this.handleSubmit.bind(this)}>
+        <input
+          className="add-ingredient-input"
+          placeholder="New Ingredient"
+          ref="newIngredient"
+        />
+      </form>
     );
   }
 }
