eb939e0babf054e200a1969ff9ecaf6a440d1bf5
Scott Woodend <woodendscott@gmail.com>
2017-02-25 19:19:28 -0500

    RecipeCard now watches for new ingredients and will update view accordingly

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -6,15 +6,31 @@ import AddIngredientInput from './AddIngredientInput';
 //this.props.num is the card number
 
 class RecipeCard extends Component {
+  constructor(props){
+    super(props);
+    this.watchNewIngredients = this.watchNewIngredients.bind(this);
+    this.state = {
+      ingredients: []
+    };
+  }
+
+  watchNewIngredients(ingredient){
+    let newIngredient = this.state.ingredients;
+    newIngredient.push(ingredient);
+    this.setState({
+      ingredients: newIngredient
+    });
+  }
+
+
   render(){
-    console.log('this.props.num', this.props.num);
     return (
       <div className="max-width">
         <div className="recipe-card">
           {this.props.children}
           <RecipeHeader title={this.props.title}/>
-          <CardBody />
-          <AddIngredientInput />
+          <CardBody ingredients={this.state.ingredients}/>
+          <AddIngredientInput addIngredient={this.watchNewIngredients}/>
         </div>
       </div>
     );
