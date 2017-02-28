ac0c6a655c4f5a477600c09995651c87b7ca3899
Scott Woodend <woodendscott@gmail.com>
2017-02-27 20:00:16 -0500

    add logic to handle case where card is edited, closed, then reopened

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import EditableItem from './EditableItem';
 
 class CardBody extends Component {
   constructor(props){
     super(props);
+    this.watchForEdits = this.watchForEdits.bind(this);
     this.state = {
-      hidden: false,
+      hidden: false
     };
+  }
+
+  watchForEdits(i, value){
+    this.props.ingredientWasEdited(i, value);
   }
 
   render(){
     let ingredients = this.props.ingredients.map((ingredient, i) => {
-      return (<li key={i}>{ingredient}</li>);
+      if (ingredient){
+        return (<EditableItem value={ingredient} key={i} num={i} watchForEdits={this.watchForEdits}/>);
+      } else {
+        return null;
+      }
     });
-    
+
     return (
       <ul className="ingredient-list">
         {ingredients}
