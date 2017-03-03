25fa2ace07fd202c931984390dbc0e3ed18759cf
Scott Woodend <woodendscott@gmail.com>
2017-03-02 22:49:15 -0500

    create a simple NavBar component to run along the top of the page

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <h1>Recipe Cards</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
